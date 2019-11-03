5ee124a827fdfdf2ab9d10ac31123e7229a4b1af
MUKHIDA Ali Ahad <mukhida8@gpu38.cse.cuhk.edu.hk>
2019-11-03 15:44:33 +0800

    Changing config file

--- configs/my_configs/reppoints_moment_r50_fpn_2x.py
+++ configs/my_configs/reppoints_moment_r50_fpn_2x.py
@@ -130,7 +130,7 @@ device_ids = range(5)
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/reppoints_moment_r50_fpn_2x'
-load_from = './work_dirs/reppoints_moment_r50_fpn_2x_train_4gpu/latest.pth'
+load_from = None #'./work_dirs/reppoints_moment_r50_fpn_2x_train_4gpu/latest.pth'
 resume_from = None
 auto_resume = True
 workflow = [('train', 1)]
