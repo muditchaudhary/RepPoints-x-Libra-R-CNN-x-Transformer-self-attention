66ba292986e1f8229c7241ecb2269897458e32ed
Mudit Chaudhary <muditchaudhary7@gmail.com>
2019-11-20 01:24:57 +0800

    Add Random Sampler

--- mmdetection/tools/trainers/slurm_train_reppoints_moment_r50_fpn_2x_BLoss_CELoss_init_1.sh
+++ mmdetection/tools/trainers/slurm_train_reppoints_moment_r50_fpn_2x_BLoss_CELoss_init_1.sh
@@ -3,12 +3,12 @@ source ../../anaconda3/etc/profile.d/conda.sh
 conda activate pytorch12 #specify conda environment
 set -x
 
-PARTITION=gpu_24h   #specify gpu time 2h, 8h, 24h
+PARTITION=gpu_2h   #specify gpu time 2h, 8h, 24h
 JOB_NAME=reppoints_moment_r50_fpn_2x_BLoss_CELoss_1 #job name can be anything   
-CONFIG=./configs/my_configs/Libra/reppoints_moment_r50_fpn_2x_BLoss_CELoss_1.py #specify the config file
-WORK_DIR=./work_dirs/reppoints_moment_r50_fpn_2x_BLoss_CELoss_1 #where to save the models
-GPUS=${5:-4}    #specify number of gpus ${5:-num_gpus} 
-GPUS_PER_NODE=${GPUS_PER_NODE:-4} #specify number of gpus per node ${:-num_gpu per node} 
+CONFIG=./configs/my_configs/Libra/reppoints_moment_r50_fpn_2x_BLoss_CELoss_1_Random_Sampler.py #specify the config file
+WORK_DIR=./work_dirs/reppoints_moment_r50_fpn_2x_BLoss_CELoss_1_Random_Sampler #where to save the models
+GPUS=${5:-2}    #specify number of gpus ${5:-num_gpus} 
+GPUS_PER_NODE=${GPUS_PER_NODE:-2} #specify number of gpus per node ${:-num_gpu per node} 
 CPUS_PER_TASK=${CPUS_PER_TASK:-5}
 SRUN_ARGS=${SRUN_ARGS:-""}
 PY_ARGS=${PY_ARGS:-"--validate"}
