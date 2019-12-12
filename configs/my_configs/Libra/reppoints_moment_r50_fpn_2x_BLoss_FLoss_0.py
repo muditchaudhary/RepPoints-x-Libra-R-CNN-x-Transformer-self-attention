5092ebf742b6d825263587561bce22e5f8274c29
Ali Ahad Mukhida <aliahad99@link.cuhk.edu.hk>
2019-12-12 15:42:16 +0500

    Update reppoints_moment_r50_fpn_2x_BLoss_FLoss_0.py

--- configs/my_configs/Libra/reppoints_moment_r50_fpn_2x_BLoss_FLoss_0.py
+++ configs/my_configs/Libra/reppoints_moment_r50_fpn_2x_BLoss_FLoss_0.py
@@ -136,6 +136,6 @@ dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/reppoints_moment_r50_fpn_2x'
 load_from = None
-resume_from = None
+resume_from = './work_dirs/reppoints_moment_r50_fpn_2x_BLoss_FLoss_0/latest.pth'
 auto_resume = True
 workflow = [('train', 1)]
