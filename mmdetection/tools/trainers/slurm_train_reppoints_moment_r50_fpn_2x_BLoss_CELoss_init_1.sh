ed5e35e74a5c954d4ca2cead26586ab86d491ef7
Mudit Chaudhary <muditchaudhary7@gmail.com>
2019-12-18 16:38:39 +0530

    Modify CELoss trainer

--- mmdetection/tools/trainers/slurm_train_reppoints_moment_r50_fpn_2x_BLoss_CELoss_init_1.sh
+++ mmdetection/tools/trainers/slurm_train_reppoints_moment_r50_fpn_2x_BLoss_CELoss_init_1.sh
@@ -3,7 +3,7 @@ source ../../anaconda3/etc/profile.d/conda.sh
 conda activate pytorch12 #specify conda environment
 set -x
 
-PARTITION=gpu_2h   #specify gpu time 2h, 8h, 24h
+PARTITION=gpu_24h   #specify gpu time 2h, 8h, 24h
 JOB_NAME=reppoints_moment_r50_fpn_2x_BLoss_CELoss_1 #job name can be anything   
 CONFIG=./configs/my_configs/Libra/reppoints_moment_r50_fpn_2x_BLoss_CELoss_1_Random_Sampler.py #specify the config file
 WORK_DIR=./work_dirs/reppoints_moment_r50_fpn_2x_BLoss_CELoss_1_Random_Sampler #where to save the models
