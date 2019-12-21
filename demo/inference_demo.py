e533938a78b81eec726de9f5e0b12e1b1d3a668a
Mudit Chaudhary <muditchaudhary7@gmail.com>
2019-12-22 00:25:46 +0530

    Add inference demo files and images

--- demo/inference_demo.py
+++ demo/inference_demo.py
@@ -6,8 +6,10 @@ import mmcv
 config_file = sys.argv[1]
 checkpoint_file = sys.argv[2]
 out_file = sys.argv[3]
-test_img = './test.jpg'
+
 model = init_detector(config_file,checkpoint_file)
 
-result = inference_detector(model,test_img)
-show_result(test_img,model.CLASSES, out_file=out_file)
+for i in range(1,11):
+    test_img = './test_'+str(i)+'.jpg'
+    result = inference_detector(model,test_img)
+    show_result(test_img,model.CLASSES, out_file=test_img+'result')
