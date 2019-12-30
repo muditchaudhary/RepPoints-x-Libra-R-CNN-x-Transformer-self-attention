a8e65b374d62a91bb32494bf7b475e946458c863
Mudit Chaudhary <muditchaudhary7@gmail.com>
2019-12-30 11:11:28 +0530

    Fix base debugger

--- configs/my_configs/Debugger/debug_base.py
+++ configs/my_configs/Debugger/debug_base.py
@@ -131,6 +131,6 @@ dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/reppoints_moment_r50_fpn_2x'
 load_from = None
-resume_from = './work_dirs/reppoints_moment_r50_fpn_2x_train_4gpu_12epoch/latest.pth'
+resume_from = None # './work_dirs/reppoints_moment_r50_fpn_2x_train_4gpu_12epoch/latest.pth'
 auto_resume = True
 workflow = [('train', 1)]
