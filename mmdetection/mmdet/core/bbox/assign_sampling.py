d58792608530b9375f58a003fd0b2e4dc6f42b94
Mudit Chaudhary <muditchaudhary7@gmail.com>
2019-12-30 11:40:34 +0530

    Add debugging statement assign_sampling

--- mmdetection/mmdet/core/bbox/assign_sampling.py
+++ mmdetection/mmdet/core/bbox/assign_sampling.py
@@ -25,6 +25,8 @@ def build_sampler(cfg, **kwargs):
 
 def assign_and_sample(bboxes, gt_bboxes, gt_bboxes_ignore, gt_labels, cfg):
     bbox_assigner = build_assigner(cfg.assigner)
+    print("-##--##-- DEBUGGER STATEMENT --##--##-")
+    print("Check cfg for sampler")
     bbox_sampler = build_sampler(cfg.sampler)
     assign_result = bbox_assigner.assign(bboxes, gt_bboxes, gt_bboxes_ignore,
                                          gt_labels)
