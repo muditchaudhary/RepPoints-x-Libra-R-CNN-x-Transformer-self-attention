a8b7ba6ba95f53035a67d81134b0a03df69013a0
Mudit Chaudhary <muditchaudhary7@gmail.com>
2020-01-15 12:15:59 +0530

    Fix trainer

--- mmdetection/tools/trainers/slurm_train_reppoints_moment_r50_fpn_2x_FLoss_1_BSampler.sh
+++ mmdetection/tools/trainers/slurm_train_reppoints_moment_r50_fpn_2x_FLoss_1_BSampler.sh
@@ -3,12 +3,12 @@ source ../../anaconda3/etc/profile.d/conda.sh
 conda activate pytorch12 #specify conda environment
 set -x
 
-PARTITION=gpu_2h   #specify gpu time 2h, 8h, 24h
+PARTITION=gpu_24h   #specify gpu time 2h, 8h, 24h
 JOB_NAME=reppoints_moment_r50_fpn_2x_BLoss_CELoss_BSampler #job name can be anything   
 CONFIG=./configs/my_configs/Libra/reppoints_moment_r50_fpn_2x_FLoss_1_BSampler.py #specify the config file
 WORK_DIR=./work_dirs/reppoints_moment_r50_fpn_2x_FLoss_1_BSampler #where to save the models
 GPUS=${5:-4}    #specify number of gpus ${5:-num_gpus}
-GPUS_PER_NODE=${GPUS_PER_NODE:-2} #specify number of gpus per node ${:-num_gpu per node} 
+GPUS_PER_NODE=${GPUS_PER_NODE:-4} #specify number of gpus per node ${:-num_gpu per node}
 CPUS_PER_TASK=${CPUS_PER_TASK:-5}
 SRUN_ARGS=${SRUN_ARGS:-""}
 PY_ARGS=${PY_ARGS:-"--validate"}
