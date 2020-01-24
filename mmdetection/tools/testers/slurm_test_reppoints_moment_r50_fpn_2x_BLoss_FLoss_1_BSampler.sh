f1001af18a1ce091455321ad97bb0cb171588ba3
Mudit Chaudhary <muditchaudhary7@gmail.com>
2020-01-24 10:17:18 +0530

    Modify tester: BSampler+FLoss1

--- mmdetection/tools/testers/slurm_test_reppoints_moment_r50_fpn_2x_BLoss_FLoss_1_BSampler.sh
+++ mmdetection/tools/testers/slurm_test_reppoints_moment_r50_fpn_2x_BLoss_FLoss_1_BSampler.sh
@@ -22,5 +22,5 @@ srun -p ${PARTITION} \
     --cpus-per-task=${CPUS_PER_TASK} \
     --kill-on-bad-exit=1 \
     ${SRUN_ARGS} \
-    python -u ./mmdetection/tools/test.py ${CONFIG} ${CHECKPOINT} --launcher="slurm" --out "./reppoints_moment_r50_fpn_2x_FLoss_1_BSampler/results.pkl" --eval "bbox"
+    python -u ./mmdetection/tools/test.py ${CONFIG} ${CHECKPOINT} --launcher="slurm" --out "./work_dirs/reppoints_moment_r50_fpn_2x_FLoss_1_BSampler/results.pkl" --eval "bbox"
     #${PY_ARGS}
