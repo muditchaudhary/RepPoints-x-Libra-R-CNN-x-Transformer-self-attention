0de54c61e623c4a666756098b3c393a84708fa31
Mudit Chaudhary <muditchaudhary7@gmail.com>
2020-02-21 15:54:37 +0530

    Debug init sampling

--- mmdetection/mmdet/core/bbox/assign_sampling.py
+++ mmdetection/mmdet/core/bbox/assign_sampling.py
@@ -28,6 +28,7 @@ def assign_and_sample(bboxes, gt_bboxes, gt_bboxes_ignore, gt_labels, cfg):
     bbox_sampler = build_sampler(cfg.sampler)
     assign_result = bbox_assigner.assign(bboxes, gt_bboxes, gt_bboxes_ignore,
                                          gt_labels)
+    from IPython import embed; embed();
     sampling_result = bbox_sampler.sample(assign_result, bboxes, gt_bboxes,
                                           gt_labels)
     return assign_result, sampling_result
