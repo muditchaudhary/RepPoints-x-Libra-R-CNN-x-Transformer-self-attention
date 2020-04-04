91a56fba10a44bf95bffb48e8b7efd8b12d23a6f
Mudit Chaudhary <muditchaudhary7@gmail.com>
2020-04-04 15:19:14 +0530

    gradcam prep

--- demo/inference_demo.py
+++ demo/inference_demo.py
@@ -10,9 +10,10 @@ outfile_path = './BLoss_FLoss0' +'/'
 
 model = init_detector(config_file,checkpoint_file)
 
-from IPython import embed; embed()
+from IPython import embed;
 for i in range(1,10):
     test_img = './test_'+str(i)+'.jpg'
     result = inference_detector(model,test_img)
+    print("After result")
     embed()
     show_result(test_img,result,model.CLASSES,0.5,show=False, out_file=outfile_path+test_img)
